b1f7961015cda44a630e60853b0d0a2a610fdf00
Shubham <subham7saurav@gmail.com>
2024-09-28 22:44:55 +0530

    Initial commit

---
+++ src/main/java/com/shubham/productserviceusingfakestore/ProductServiceUsingFakeStoreApplication.java
@@ -0,0 +1,19 @@
+package com.shubham.productserviceusingfakestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServiceUsingFakeStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServiceUsingFakeStoreApplication.class, args);
+	}
+
+
+	/**
+	 *
+	 *
+	 *
+	 */
+}
---
+++ src/test/java/com/shubham/productserviceusingfakestore/ProductServiceUsingFakeStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shubham.productserviceusingfakestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceUsingFakeStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
